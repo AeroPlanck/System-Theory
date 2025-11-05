b4f16ec830a0a008ce34692f69a752dd8bb0c6a1
Aero Planck <aeroplanck@gmail.com>
2025-11-05 18:42:15 +0800

    Update

--- Frustration Induced Lattice/mp4Multi.py
+++ Frustration Induced Lattice/mp4Multi.py
@@ -76,12 +76,12 @@ def draw_frame(sa: StateAnalysis):
 if __name__ == "__main__":
 
     model = CollisionBoundaryPatternFormation(
-        strengthK=20, distanceD0=1, phaseLagA0=0.4 * np.pi,
+        strengthK=20, distanceD0=1, phaseLagA0=0.99 * np.pi,
         # initPhaseTheta=np.zeros(1000), 
         omegaMin=0, deltaOmega=3,
         agentsNum=1000, dt=0.005,
         tqdm=True, savePath=SAVE_PATH, shotsnaps=10, 
-        randomSeed=10, overWrite=False
+        randomSeed=9, overWrite=False
     )
 
     # model = PhaseLagPatternFormation1D(strengthK=20, distanceD0=1, phaseLagA0=0.6*np.pi, 
